4e0911a676bb8f86f043b8a2af9d612c74208989
yindaheng98 <yindaheng98@gmail.com>
2024-11-09 20:56:08 -0800

    rm utils

--- coarse_init_infer.py
+++ coarse_init_infer.py
@@ -15,10 +15,78 @@ from dust3r.model import AsymmetricCroCo3DStereo
 from dust3r.utils.device import to_numpy
 from dust3r.image_pairs import make_pairs
 from dust3r.cloud_opt import global_aligner, GlobalAlignerMode
-from instant_splat.initializer.colmap_utils import save_colmap_cameras, save_colmap_images
 from instant_splat.initializer.dust3r.alignment import compute_global_alignment
 from instant_splat.initializer import Dust3rInitializer, InitializedCameraDataset
 from gaussian_splatting.dataset.colmap import ColmapCameraDataset
+
+######################## dust3r_utils.py ########################
+
+
+def R_to_quaternion(R):
+    """
+    Convert a rotation matrix to a quaternion.
+
+    Parameters:
+    - R: A 3x3 numpy array representing a rotation matrix.
+
+    Returns:
+    - A numpy array representing the quaternion [w, x, y, z].
+    """
+    m00, m01, m02 = R[0, 0], R[0, 1], R[0, 2]
+    m10, m11, m12 = R[1, 0], R[1, 1], R[1, 2]
+    m20, m21, m22 = R[2, 0], R[2, 1], R[2, 2]
+    trace = m00 + m11 + m22
+
+    if trace > 0:
+        s = 0.5 / np.sqrt(trace + 1.0)
+        w = 0.25 / s
+        x = (m21 - m12) * s
+        y = (m02 - m20) * s
+        z = (m10 - m01) * s
+    elif (m00 > m11) and (m00 > m22):
+        s = np.sqrt(1.0 + m00 - m11 - m22) * 2
+        w = (m21 - m12) / s
+        x = 0.25 * s
+        y = (m01 + m10) / s
+        z = (m02 + m20) / s
+    elif m11 > m22:
+        s = np.sqrt(1.0 + m11 - m00 - m22) * 2
+        w = (m02 - m20) / s
+        x = (m01 + m10) / s
+        y = 0.25 * s
+        z = (m12 + m21) / s
+    else:
+        s = np.sqrt(1.0 + m22 - m00 - m11) * 2
+        w = (m10 - m01) / s
+        x = (m02 + m20) / s
+        y = (m12 + m21) / s
+        z = 0.25 * s
+
+    return np.array([w, x, y, z])
+
+
+def save_colmap_cameras(ori_size, intrinsics, camera_file):
+    with open(camera_file, 'w') as f:
+        for i, K in enumerate(intrinsics, 1):  # Starting index at 1
+            width, height = ori_size
+            scale_factor_x = width/2 / K[0, 2]
+            scale_factor_y = height/2 / K[1, 2]
+            # assert scale_factor_x==scale_factor_y, "scale factor is not same for x and y"
+            print(f'scale factor is not same for x{scale_factor_x} and y {scale_factor_y}')
+            f.write(f"{i} PINHOLE {width} {height} {K[0, 0]*scale_factor_x} {K[1, 1]*scale_factor_x} {width/2} {height/2}\n")  # scale focal
+            # f.write(f"{i} PINHOLE {width} {height} {K[0, 0]} {K[1, 1]} {K[0, 2]} {K[1, 2]}\n")
+
+
+def save_colmap_images(poses, images_file, train_img_list):
+    with open(images_file, 'w') as f:
+        for i, pose in enumerate(poses, 1):  # Starting index at 1
+            # breakpoint()
+            pose = np.linalg.inv(pose)
+            R = pose[:3, :3]
+            t = pose[:3, 3]
+            q = R_to_quaternion(R)  # Convert rotation matrix to quaternion
+            f.write(f"{i} {q[0]} {q[1]} {q[2]} {q[3]} {t[0]} {t[1]} {t[2]} {i} {train_img_list[i-1]}\n")
+            f.write(f"\n")
 
 
 def load_images(folder_or_list, size, square_ok=False):
@@ -80,6 +148,8 @@ def storePly(path, xyz, rgb):
     vertex_element = PlyElement.describe(elements, 'vertex')
     ply_data = PlyData([vertex_element])
     ply_data.write(path)
+
+######################## dust3r_utils.py ########################
 
 
 def get_args_parser():
