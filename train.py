da6c69cfb2a6bb1d94d222d024543d6e5c42c07d
yindaheng98 <yindaheng98@gmail.com>
2024-12-15 21:06:24 -0800

    organize as function

--- train.py
+++ train.py
@@ -1,34 +1,19 @@
 import os
 import shutil
-import random
+from typing import Tuple
 import torch
-from tqdm import tqdm
-from argparse import ArgumentParser, Namespace
-from gaussian_splatting import CameraTrainableGaussianModel
-from gaussian_splatting.dataset import TrainableCameraDataset
-from gaussian_splatting.utils import psnr
+from gaussian_splatting import GaussianModel, CameraTrainableGaussianModel
+from gaussian_splatting.dataset import CameraDataset, TrainableCameraDataset
+from gaussian_splatting.trainer import AbstractTrainer
 from gaussian_splatting.dataset.colmap import ColmapTrainableCameraDataset
+from gaussian_splatting.train import save_cfg_args, training
 from instantsplat.trainer import Trainer
 from instantsplat.initializer import TrainableInitializedCameraDataset
 
 from instantsplat.initialize import initialize
 
-parser = ArgumentParser()
-parser.add_argument("--sh_degree", default=3, type=int)
-parser.add_argument("-s", "--source", required=True, type=str)
-parser.add_argument("-d", "--destination", required=True, type=str)
-parser.add_argument("-i", "--iteration", default=30000, type=int)
-parser.add_argument("-l", "--load_ply", default=None, type=str)
-parser.add_argument("--load_camera", default=None, type=str)
-parser.add_argument("--save_iterations", nargs="+", type=int, default=[7000, 30000])
-parser.add_argument("--device", default="cuda", type=str)
-parser.add_argument("-o", "--option", default=[], action='append', type=str)
 
-parser.add_argument("--init", choices=["dust3r", "colmap-sparse", "colmap-dense"], default=None, type=str)
-parser.add_argument("--init_option", default=[], action='append', type=str)
-
-
-def prepare_training(sh_degree: int, source: str, destination: str, device: str, load_ply: str = None, load_camera: str = None, configs={}, init=None, init_configs={}):
+def prepare_training(sh_degree: int, source: str, destination: str, device: str, load_ply: str = None, load_camera: str = None, configs={}, init=None, init_configs={}) -> Tuple[CameraDataset, GaussianModel, AbstractTrainer]:
     gaussians = CameraTrainableGaussianModel(sh_degree).to(device)
     if load_ply:  # load a trained model
         gaussians.load_ply(load_ply)
@@ -64,48 +49,32 @@ def prepare_training(sh_degree: int, source: str, destination: str, device: str,
     return dataset, gaussians, trainer
 
 
-def main(sh_degree: int, source: str, destination: str, iteration: int, device: str, args):
-    os.makedirs(destination, exist_ok=True)
-    with open(os.path.join(destination, "cfg_args"), 'w') as cfg_log_f:
-        cfg_log_f.write(str(Namespace(sh_degree=sh_degree, source_path=source)))
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("--sh_degree", default=3, type=int)
+    parser.add_argument("-s", "--source", required=True, type=str)
+    parser.add_argument("-d", "--destination", required=True, type=str)
+    parser.add_argument("-i", "--iteration", default=30000, type=int)
+    parser.add_argument("-l", "--load_ply", default=None, type=str)
+    parser.add_argument("--load_camera", default=None, type=str)
+    parser.add_argument("--save_iterations", nargs="+", type=int, default=[7000, 30000])
+    parser.add_argument("--device", default="cuda", type=str)
+    parser.add_argument("-o", "--option", default=[], action='append', type=str)
+    parser.add_argument("--init", choices=["dust3r", "colmap-sparse", "colmap-dense"], default=None, type=str)
+    parser.add_argument("--init_option", default=[], action='append', type=str)
+    args = parser.parse_args()
+    save_cfg_args(args.destination, args.sh_degree, args.source)
+    torch.autograd.set_detect_anomaly(False)
+
     configs = {o.split("=", 1)[0]: eval(o.split("=", 1)[1]) for o in args.option}
     init_configs = {o.split("=", 1)[0]: eval(o.split("=", 1)[1]) for o in args.init_option}
     dataset, gaussians, trainer = prepare_training(
-        sh_degree=sh_degree, source=source, destination=destination, device=device,
+        sh_degree=args.sh_degree, source=args.source, destination=args.destination, device=args.device,
         load_ply=args.load_ply, load_camera=args.load_camera, configs=configs,
         init=args.init, init_configs=init_configs)
-    dataset.save_cameras(os.path.join(destination, "cameras_orig.json"))
-
-    pbar = tqdm(range(1, iteration+1))
-    epoch = list(range(len(dataset)))
-    epoch_psnr = torch.empty(3, 0, device=device)
-    ema_loss_for_log = 0.0
-    avg_psnr_for_log = 0.0
-    for step in pbar:
-        epoch_idx = step % len(dataset)
-        if epoch_idx == 0:
-            avg_psnr_for_log = epoch_psnr.mean().item()
-            epoch_psnr = torch.empty(3, 0, device=device)
-            random.shuffle(epoch)
-        idx = epoch[epoch_idx]
-        loss, out = trainer.step(dataset[idx])
-        with torch.no_grad():
-            ema_loss_for_log = 0.4 * loss.item() + 0.6 * ema_loss_for_log
-            epoch_psnr = torch.concat([epoch_psnr, psnr(out["render"], dataset[idx].ground_truth_image)], dim=1)
-            if step % 10 == 0:
-                pbar.set_postfix({'epoch': step // len(dataset), 'loss': ema_loss_for_log, 'psnr': avg_psnr_for_log, 'n': gaussians._xyz.shape[0]})
-        if step in args.save_iterations:
-            save_path = os.path.join(destination, "point_cloud", "iteration_" + str(step))
-            os.makedirs(save_path, exist_ok=True)
-            gaussians.save_ply(os.path.join(save_path, "point_cloud.ply"))
-            dataset.save_cameras(os.path.join(destination, "cameras.json"))
-    save_path = os.path.join(destination, "point_cloud", "iteration_" + str(iteration))
-    os.makedirs(save_path, exist_ok=True)
-    gaussians.save_ply(os.path.join(save_path, "point_cloud.ply"))
-    dataset.save_cameras(os.path.join(destination, "cameras.json"))
-
-
-if __name__ == "__main__":
-    args = parser.parse_args()
-    torch.autograd.set_detect_anomaly(False)
-    main(args.sh_degree, args.source, args.destination, args.iteration, args.device, args)
+    dataset.save_cameras(os.path.join(args.destination, "cameras.json"))
+    training(
+        dataset=dataset, gaussians=gaussians, trainer=trainer,
+        destination=args.destination, iteration=args.iteration, save_iterations=args.save_iterations,
+        device=args.device)
