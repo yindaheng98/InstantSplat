8bd47e6055e3687d37fa4a6ad23a772c4aa2632b
yindaheng98 <yindaheng98@gmail.com>
2025-03-23 22:23:10 -0700

    scale ok

--- instantsplat/initializer/align.py
+++ instantsplat/initializer/align.py
@@ -5,13 +5,19 @@ from .abc import AbstractInitializer, InitializingCamera, InitializedPointCloud
 
 
 def align_cameras(reference_points: torch.Tensor, reference_cameras: List[InitializingCamera], cameras: List[InitializingCamera]) -> torch.Tensor:
+    reference_cameras = sorted(reference_cameras, key=lambda camera: camera.image_path)
+    cameras = sorted(cameras, key=lambda camera: camera.image_path)
     R_ref = torch.stack([camera.R for camera in reference_cameras])
     T_ref = torch.stack([camera.T for camera in reference_cameras])
     R = torch.stack([camera.R for camera in cameras]).to(R_ref.dtype)
     T = torch.stack([camera.T for camera in cameras]).to(T_ref.dtype)
     R_tran = torch.bmm(R_ref.transpose(1, 2), R).mean(0)
-    # TODO: implement translation alignment
-    return reference_points @ R_tran.T.to(reference_points.dtype)
+    dist_T = (T.unsqueeze(0) - T.unsqueeze(1)).norm(dim=-1, p=2)
+    dist_T_ref = (T_ref.unsqueeze(0) - T_ref.unsqueeze(1)).norm(dim=-1, p=2)
+    scales = dist_T_ref / dist_T
+    scale = scales[~scales.isnan()].mean()
+    return reference_points @ R_tran.T.to(reference_points.dtype) * scale
+
 
 
 class AlignInitializer(AbstractInitializer):
