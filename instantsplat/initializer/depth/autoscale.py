a8c895a672bf4b73d5a43d210bcdc2c844e05195
yindaheng98 <yindaheng98@gmail.com>
2025-04-20 22:47:13 -0700

    compute uvd

--- instantsplat/initializer/depth/autoscale.py
+++ instantsplat/initializer/depth/autoscale.py
@@ -1,10 +1,15 @@
 import abc
+import math
 from typing import List
 
 import torch
 
 from instantsplat.initializer import InitializedPointCloud, InitializingCamera
 from .abc import DepthInitializerWrapper
+
+
+def fov2focal(fov, pixels):
+    return pixels / (2 * math.tan(fov / 2))
 
 
 class AutoScaleDepthInitializerWrapper(DepthInitializerWrapper):
@@ -15,7 +20,17 @@ class AutoScaleDepthInitializerWrapper(DepthInitializerWrapper):
         raise NotImplementedError("Subclasses should implement this method.")
 
     def autoscale_depth(self, raw_depth: torch.Tensor, pointcloud: InitializedPointCloud, camera: InitializingCamera) -> torch.Tensor:
-        pass
+        xyz = pointcloud.points
+        fx, fy = fov2focal(camera.FoVx, camera.image_width), fov2focal(camera.FoVy, camera.image_height)
+        cx, cy = camera.image_width / 2, camera.image_height / 2
+        K = torch.tensor([[fx, 0, cx], [0, fy, cy], [0, 0, 1]], device=raw_depth.device)
+        R = camera.R.to(dtype=xyz.dtype)
+        T = camera.T.to(dtype=xyz.dtype)
+        uvd = (K @ ((R @ xyz.T).T + T.unsqueeze(0)).T).T
+        uv, d = uvd[:, 0:2] / uvd[:, 2:3], uvd[:, 2:3]
+        valid_mask = (0 <= uv[:, 0:2]).all(-1) & (uv[:, 0] < camera.image_width) & (uv[:, 1] < camera.image_height)
+        uv, d = uv[valid_mask, :], d[valid_mask, :]
+        return raw_depth
 
     def compute_depths(self, pointcloud: InitializedPointCloud, cameras: List[InitializingCamera]) -> List[torch.Tensor]:
         """Compute depth and auto scale according to the point cloud and cameras."""
--- instantsplat/initializer/depth/depth_anything_v2.py
+++ instantsplat/initializer/depth/depth_anything_v2.py
@@ -35,22 +35,25 @@ class DepthAnythingV2InitializerWrapper(AutoScaleDepthInitializerWrapper):
             input_size=518,
             encoder='vitl',
             checkpoints_folder='checkpoints',
-            image_path_to_depth_path=None):
+            image_path_to_depth_path=None,
+            device="cuda"):
         super().__init__(base_initializer)
         self.input_size = input_size
         depth_anything = DepthAnythingV2(**model_configs[encoder])
         depth_anything.load_state_dict(torch.load(os.path.join(checkpoints_folder, f'depth_anything_v2_{encoder}.pth'), map_location='cpu'))
         self.depth_anything = depth_anything.eval()
         self.image_path_to_depth_path = default_image_path_to_depth_path if image_path_to_depth_path is None else image_path_to_depth_path
+        self.to(device)
 
     def to(self, device):
+        self.device = device
         self.depth_anything = self.depth_anything.to(device).eval()
         return super().to(device)
 
     def compute_depth(self, image_path: str) -> torch.Tensor:
         depth_anything = self.depth_anything
         raw_image = cv2.imread(image_path)
-        return torch.tensor(depth_anything.infer_image(raw_image, self.input_size))
+        return torch.tensor(depth_anything.infer_image(raw_image, self.input_size), device=self.device).unsqueeze(0)
 
     def compute_raw_depths(self, pointcloud: InitializedPointCloud, cameras: List[InitializingCamera]) -> List[torch.Tensor]:
         return [self.compute_depth(camera.image_path) for camera in tqdm.tqdm(cameras, desc="Computing Depths")]
