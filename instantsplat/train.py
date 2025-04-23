0d5badae160191e6c532cbc6cf9700b68496a405
yindaheng98 <yindaheng98@gmail.com>
2025-04-23 00:40:10 -0700

    use mode to choose trainer

--- instantsplat/train.py
+++ instantsplat/train.py
@@ -10,30 +10,36 @@ from gaussian_splatting.train import save_cfg_args, training
 from instantsplat.trainer import Trainer, BaseTrainer
 from instantsplat.trainer import ScaleRegularizeTrainer, BaseScaleRegularizeTrainer
 from instantsplat.initializer import TrainableInitializedCameraDataset
+from instantsplat.initialize import initialize
 
-from .initialize import initialize
+basemodes = {
+    "base": Trainer,
+    "nodepth": BaseTrainer,
+}
+scaleregmodes = {
+    "base": ScaleRegularizeTrainer,
+    "nodepth": BaseScaleRegularizeTrainer,
+}
 
 
-def prepare_training(sh_degree: int, source: str, destination: str, device: str, load_ply: str = None, load_camera: str = None, with_depth=False, with_scale_reg=False, configs={}, init=None, init_configs={}) -> Tuple[CameraDataset, GaussianModel, AbstractTrainer]:
+def prepare_training(sh_degree: int, source: str, destination: str, device: str, mode: str, load_ply: str = None, load_camera: str = None, load_depth=False, with_scale_reg=False, configs={}, init=None, init_configs={}) -> Tuple[CameraDataset, GaussianModel, AbstractTrainer]:
     gaussians = CameraTrainableGaussianModel(sh_degree).to(device)
     if init:  # initialize
-        initialized_cameras, initialized_point_cloud = initialize(initializer=init, directory=source, with_depth=True, configs=init_configs, device=device)
+        initialized_cameras, initialized_point_cloud = initialize(initializer=init, directory=source, with_depth=load_depth, configs=init_configs, device=device)
         dataset = TrainableInitializedCameraDataset(initialized_cameras).to(device)
         gaussians.create_from_pcd(initialized_point_cloud.points, initialized_point_cloud.colors)
         if os.path.exists(os.path.join(destination, "input.ply")):
             os.remove(os.path.join(destination, "input.ply"))
         initialized_point_cloud.save_ply(os.path.join(destination, "input.ply"))
     else:  # create_from_pcd
-        dataset = (TrainableCameraDataset.from_json(load_camera, load_depth=True) if load_camera else ColmapTrainableCameraDataset(source, load_depth=True)).to(device)
+        dataset = (TrainableCameraDataset.from_json(load_camera, load_depth=load_depth) if load_camera else ColmapTrainableCameraDataset(source, load_depth=load_depth)).to(device)
         colmap_init(gaussians, source) if not load_ply else gaussians.load_ply(load_ply)
         if os.path.exists(os.path.join(destination, "input.ply")):
             os.remove(os.path.join(destination, "input.ply"))
         shutil.copy2(os.path.join(source, "sparse/0", "points3D.ply"), os.path.join(destination, "input.ply"))
 
-    trainer = {
-        True: {False: Trainer, True: ScaleRegularizeTrainer},
-        False: {False: BaseTrainer, True: BaseScaleRegularizeTrainer},
-    }[with_depth][with_scale_reg](
+    modes = basemodes if not with_scale_reg else scaleregmodes
+    trainer = modes[mode](
         gaussians,
         scene_extent=dataset.scene_extent(),
         dataset=dataset,
@@ -51,8 +57,9 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--iteration", default=1000, type=int)
     parser.add_argument("-l", "--load_ply", default=None, type=str)
     parser.add_argument("--load_camera", default=None, type=str)
-    parser.add_argument("--with_depth", action="store_true")
+    parser.add_argument("--no_depth_data", action='store_true')
     parser.add_argument("--with_scale_reg", action="store_true")
+    parser.add_argument("--mode", choices=sorted(list(set(list(basemodes.keys()) + list(scaleregmodes.keys())))), default="base")
     parser.add_argument("--save_iterations", nargs="+", type=int, default=[1000])
     parser.add_argument("--device", default="cuda", type=str)
     parser.add_argument("-o", "--option", default=[], action='append', type=str)
@@ -65,8 +72,8 @@ if __name__ == "__main__":
     configs = {o.split("=", 1)[0]: eval(o.split("=", 1)[1]) for o in args.option}
     init_configs = {o.split("=", 1)[0]: eval(o.split("=", 1)[1]) for o in args.init_option}
     dataset, gaussians, trainer = prepare_training(
-        sh_degree=args.sh_degree, source=args.source, destination=args.destination, device=args.device,
-        load_ply=args.load_ply, load_camera=args.load_camera, with_depth=args.with_depth, with_scale_reg=args.with_scale_reg, configs=configs,
+        sh_degree=args.sh_degree, source=args.source, destination=args.destination, device=args.device, mode=args.mode,
+        load_ply=args.load_ply, load_camera=args.load_camera, load_depth=not args.no_depth_data, with_scale_reg=args.with_scale_reg, configs=configs,
         init=args.init, init_configs=init_configs)
     dataset.save_cameras(os.path.join(args.destination, "cameras.json"))
     training(
