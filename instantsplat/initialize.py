f00ebe7b6db4f9ee005fbf6077ab568f9631f4bb
yindaheng98 <yindaheng98@gmail.com>
2025-04-25 18:08:27 -0700

    use match

--- instantsplat/initialize.py
+++ instantsplat/initialize.py
@@ -15,18 +15,19 @@ default_image_folder = {
 def initialize(initializer, directory, configs, device):
     image_folder = os.path.join(directory, default_image_folder[initializer])
     image_path_list = [os.path.join(image_folder, file) for file in sorted(os.listdir(image_folder))]
-    if initializer == "dust3r":
-        initializer = DepthAnythingV2Dust3rInitializer(**configs).to(device)
-    elif initializer == "mast3r":
-        initializer = DepthAnythingV2Mast3rInitializer(**configs).to(device)
-    elif initializer == "colmap-sparse":
-        initializer = DepthAnythingV2ColmapSparseInitializer(destination=directory, **configs).to(device)
-    elif initializer == "colmap-dense":
-        initializer = DepthAnythingV2ColmapDenseInitializer(destination=directory, **configs).to(device)
-    elif initializer == "dust3r-align-colmap":
-        initializer = DepthAnythingV2Dust3rAlign2ColmapDenseInitializer(destination=directory, **configs).to(device)
-    else:
-        raise ValueError(f"Unknown initializer {initializer}")
+    match initializer:
+        case "dust3r":
+            initializer = DepthAnythingV2Dust3rInitializer(**configs).to(device)
+        case "mast3r":
+            initializer = DepthAnythingV2Mast3rInitializer(**configs).to(device)
+        case "colmap-sparse":
+            initializer = DepthAnythingV2ColmapSparseInitializer(destination=directory, **configs).to(device)
+        case "colmap-dense":
+            initializer = DepthAnythingV2ColmapDenseInitializer(destination=directory, **configs).to(device)
+        case "dust3r-align-colmap":
+            initializer = DepthAnythingV2Dust3rAlign2ColmapDenseInitializer(destination=directory, **configs).to(device)
+        case _:
+            raise ValueError(f"Unknown initializer {initializer}")
     initialized_point_cloud, initialized_cameras = initializer(image_path_list=image_path_list)
     return initialized_cameras, initialized_point_cloud
 
