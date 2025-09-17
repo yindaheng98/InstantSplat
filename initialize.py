17059fb6efb3cbce79e74b8470a31ea0866852ff
yindaheng98 <yindaheng98@gmail.com>
2025-09-17 05:34:31 -0700

    better initialize

--- initialize.py
+++ initialize.py
@@ -7,19 +7,32 @@ from tqdm import tqdm
 from instantsplat.initialize import *
 
 default_image_folder = {
+    "dust3r": "images",
     "colmap-sparse": "input",
     "colmap-dense": "input",
-    "dust3r-align-colmap": "input",
-    "nodepth-colmap-sparse": "input",
-    "nodepth-colmap-dense": "input",
-    "nodepth-dust3r-align-colmap": "input",
 }
+
+
+def initialize(initializer, directory, configs, device):
+    image_folder = os.path.join(directory, default_image_folder[initializer])
+    image_path_list = [os.path.join(image_folder, file) for file in sorted(os.listdir(image_folder))]
+    match initializer:
+        case "dust3r":
+            initializer = Dust3rInitializer(**configs).to(device)
+        case "colmap-sparse":
+            initializer = ColmapSparseInitializer(destination=directory, **configs).to(device)
+        case "colmap-dense":
+            initializer = ColmapDenseInitializer(destination=directory, **configs).to(device)
+        case _:
+            raise ValueError(f"Unknown initializer {initializer}")
+    initialized_point_cloud, initialized_cameras = initializer(image_path_list=image_path_list)
+    return initialized_cameras, initialized_point_cloud
 
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
     parser = ArgumentParser()
-    parser.add_argument("-i", "--initializer", choices=list(default_image_folder.keys()), default="dust3r", type=str)
+    parser.add_argument("-i", "--initializer", choices=["colmap-sparse", "colmap-dense"], default="colmap-dense", type=str)
     parser.add_argument("-d", "--directory", required=True, type=str)
     parser.add_argument("--device", default="cuda", type=str)
     parser.add_argument("-o", "--option", default=[], action='append', type=str)
