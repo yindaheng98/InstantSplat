8ef6c5b6229be7747e987dc1a163618bd0d29ebc
Isaac Ding <isaac.ding@gmail.com>
2025-11-04 20:53:25 -0800

    minor bug fix

--- test.py
+++ test.py
@@ -1,21 +1,38 @@
 import os
 import subprocess
 
-def initialize(name, folder, clean = True):
+def initialize(name, folder, clean = True, max_search_frames = 255):
     colmap_location = "\'/home/isaac/miniconda3/envs/gs2/bin/colmap\'"
     input_path = os.path.join(folder, name)
     if clean:
         import shutil
-        directories = []
-        directories.append(os.path.join(input_path, "distorted"))
-        directories.append(os.path.join(input_path, "sparse"))
-        directories.append(os.path.join(input_path, "stereo"))
-        directories.append(os.path.join(input_path, "fused.ply"))
-        directories.append(os.path.join(input_path, "fused.ply.vis"))
-        directories.append(os.path.join(input_path, "fused.ply.vis"))
-        # TODO: finish function
+        paths= []
+        paths.append(os.path.join(input_path, "background"))
+        paths.append(os.path.join(input_path, "distorted"))
+        paths.append(os.path.join(input_path, "images"))
+        paths.append(os.path.join(input_path, "sparse"))
+        paths.append(os.path.join(input_path, "stereo"))
+        paths.append(os.path.join(input_path, "point_cloud"))
+        paths.append(os.path.join(input_path, "fused.ply"))
+        paths.append(os.path.join(input_path, "fused.ply.vis"))
+        paths.append(os.path.join(input_path, "input.ply"))
+        paths.append(os.path.join(input_path, "run-colmap-geometric.sh"))
+        paths.append(os.path.join(input_path, "run-colmap-photometric.sh"))
+        paths.append(os.path.join(input_path, "cameras.json"))
+        paths.append(os.path.join(input_path, "cfg_args"))
 
-    command = f"python ./initialize.py -d {input_path} --initializer colmap-dense -ocolmap_executable={colmap_location} -oallow_undistortion_missing=1 -ouse_fused=True"
+        for search_idx in range(max_search_frames):
+            search_path = os.path.join(input_path, f"frame{search_idx}")
+            paths.append(search_path)
+        
+        for path in paths:
+            if os.path.exists(path):
+                if os.path.isfile(path):
+                    os.remove(path)
+                else:
+                    shutil.rmtree(path)
+
+    command = f"python ./initialize.py -d {input_path} --initializer colmap-dense -oallow_undistortion_missing=1 -ouse_fused=True"
     print("running command:")
     print(command)
     subprocess.check_call(command, shell=True)
@@ -31,7 +48,7 @@ def reinitialize(name, data_folder, background_folder):
 def train_background(name, data_folder, background_folder, iterations):
     input_path = os.path.join(data_folder, name)
     output_path = os.path.join(background_folder, name)
-    command = f"python -m reduced_3dgs.train -s {input_path} -d {output_path} -i {iterations} --mode camera-densify-prun-shculling --with_scale_reg --empty_cache_every_step --no_depth_data"
+    command = f"python -m reduced_3dgs.train -s {input_path} -d {output_path} -i {iterations} --mode camera-densify-prune-shculling --with_scale_reg --empty_cache_every_step --no_depth_data"
     print("running command:")
     print(command)
     subprocess.check_call(command, shell=True)
@@ -57,7 +74,7 @@ def train_initialized(name, input_folder, output_folder, train_indices, iteratio
             command = f"python -m instantsplat.train -s {source_location} -d {destination_location} -i {iterations} --with_scale_reg "
         else:
             background_location = os.path.join(background_folder, name, f"point_cloud/iteration_{background_iterations}/point_cloud.ply")
-            command = f"python -m instantsplat.train -s {source_location} -d {destination_location} -b {background_location} -i {iterations} -ocamera_position_lr_init=0.0 -ocamera_position_lr_final=0.0 -ocamera_rotation_lr_init=0.0 -ocamera_rotation_lr_final=0.0 -ocamera_exposure_lr_init=0.0 -ocamera_exposure_lr_final=0.0 --with_scale_reg"
+            command = f"python -m instantsplat.train -s {source_location} -d {destination_location} -i {iterations} --with_scale_reg -ocamera_position_lr_init=0 -ocamera_position_lr_final=0 -ocamera_rotation_lr_init=0 -ocamera_rotation_lr_final=0 --load_background {background_location}"
         print("running command:")
         print(command)
         subprocess.check_call(command, shell=True)
@@ -82,7 +99,10 @@ def complete_process(name, data_folder, output_folder, background_folder, foregr
 if __name__ == "__main__":
     align_name = "aligned_frames5"
     data_folder = "./data"
-    background_folder = "./data/backgrounds"
     output_folder = "./output"
-    iterations = 1000
-    complete_process()
+    complete_process(name = align_name,
+                     data_folder = data_folder,
+                     output_folder = output_folder,
+                     background_folder = os.path.join(output_folder, "backgrounds"),
+                     foreground_iterations = 1000,
+                     background_iterations = 30000)
